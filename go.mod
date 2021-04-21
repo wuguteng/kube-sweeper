ff2313193354aae3b270b019a1dc314d76da0b42
kevinyjn <kevinyjn@gmail.com>
2021-04-21 10:26:30 +0800

    upgrade dependency libraries

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.15
 
 require (
 	github.com/kataras/iris v11.1.1+incompatible
-	github.com/kevinyjn/gocom v0.2.6-0.20210227102955-a8d9352194e1
+	github.com/kevinyjn/gocom v0.2.6-0.20210421022132-14d6d3da93c4
 )
--- unittests/filelogcleaner_test.go
+++ unittests/filelogcleaner_test.go
@@ -6,7 +6,7 @@ import (
 	"regexp"
 	"testing"
 
-	"github.com/kevinyjn/gocom/unittests"
+	"github.com/kevinyjn/gocom/testingutil"
 	"wuguteng.com/kube-sweeper/sweeper"
 )
 
@@ -16,22 +16,22 @@ func TestMatchCleanLogFileName(t *testing.T) {
 	kubeAPICaPath := ""
 	kubeAPIToken := ""
 	r, err := regexp.Compile(sweeper.LogPathExtractPattern)
-	unittests.AssertNil(t, err, "regexp.Compile err")
-	unittests.AssertNotNil(t, r, "regexp.Compile regex")
+	testingutil.AssertNil(t, err, "regexp.Compile err")
+	testingutil.AssertNotNil(t, r, "regexp.Compile regex")
 	r2, err := regexp.Compile(sweeper.LogPathPodNameExtractPattern)
-	unittests.AssertNil(t, err, "regexp.Compile err")
-	unittests.AssertNotNil(t, r2, "regexp.Compile regex")
+	testingutil.AssertNil(t, err, "regexp.Compile err")
+	testingutil.AssertNotNil(t, r2, "regexp.Compile regex")
 	fis, err := ioutil.ReadDir(logPath)
-	unittests.AssertNil(t, err, "ioutil.ReadDir err")
-	unittests.AssertNotNil(t, fis, "ioutil.ReadDir files")
+	testingutil.AssertNil(t, err, "ioutil.ReadDir err")
+	testingutil.AssertNotNil(t, fis, "ioutil.ReadDir files")
 	for _, fi := range fis {
 		if fi.IsDir() {
 			podInfo, err := sweeper.ExtractPodInfoFromFileName(r, r2, fi.Name())
-			unittests.AssertNil(t, err, "ExtractPodInfoFromFileName err")
+			testingutil.AssertNil(t, err, "ExtractPodInfoFromFileName err")
 			fmt.Printf("match result for %s is: %+v\n", fi.Name(), podInfo)
 			if "" != kubeAPIServer {
 				status, err := sweeper.GetKubernetesPodState(podInfo, kubeAPIServer, kubeAPICaPath, kubeAPIToken)
-				unittests.AssertNil(t, err, "GetKubernetesPodState err")
+				testingutil.AssertNil(t, err, "GetKubernetesPodState err")
 				fmt.Printf("status of pod:%s is:%+v\n", podInfo.PodName, status)
 			}
 		}
